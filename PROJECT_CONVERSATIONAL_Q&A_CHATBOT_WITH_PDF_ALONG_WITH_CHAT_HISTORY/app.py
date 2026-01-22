1d4f3844ddea9c3738a32099eabe39c0564f2f12
Ratnesh0301 <bhardwajratnesh3@gmail.com>
2026-01-22 18:30:34 +0530

    retreival_chains

--- PROJECT_CONVERSATIONAL_Q&A_CHATBOT_WITH_PDF_ALONG_WITH_CHAT_HISTORY/app.py
+++ PROJECT_CONVERSATIONAL_Q&A_CHATBOT_WITH_PDF_ALONG_WITH_CHAT_HISTORY/app.py
@@ -4,7 +4,8 @@ from langchain_core.prompts import ChatPromptTemplate, MessagePlaceholder
 from langchain_core.chat_history import BaseChatMessageHistory
 from langchain_chroma import Chroma
 from langchain_classic.chains.combine_documents import create_stuff_documents_chain
-from langchain_classic.chains.conversational_retrieval import create
+from langchain_classic.chains.history_aware_retriever import create_history_aware_retriever
+from langchain_classic.chains.retrieval import create_retrieval_chain
 from langchain_community.chat_message_histories import ChatMessageHistory
 from langchain_groq import ChatGroq
 from langchain_huggingface import HuggingFaceEmbeddings
@@ -67,5 +68,25 @@ if api_key:
             ]
         )
 
-#        history_aware_retriever = 
+        history_aware_retriever = create_history_aware_retriever(llm,retriever,contextualize_q_prompt)
+
+        #Answer Questions
+        system_prompt = (
+            """You're are an assistant for question answering tasks.
+            Use the following pieces of retrieved context to asnwer the question.
+            if you don't know the answer, say that you don't know the answer.
+            Use three sentences maximum and keep the answer concise
+            \n\n
+            {context}
+            """
+        )
+
+        qa_prompt = ChatPromptTemplate.from_messages(
+            [('system',system_prompt),
+            MessagePlaceholder('chat_history'),
+            ('human','{input}')
+        ])
+
+        question_asnwer_chain = create_stuff_documents_chain(llm,qa_prompt)
+        rag_chain = create_retrieval_chain(history_aware_retriever, question_asnwer_chain)
 
